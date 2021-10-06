326d7bba9d063bd157b2c62ba8c43a9927313189
urbanrustic <ankershtok77@gmail.com>
2021-10-06 18:56:58 +0300

    begin

--- M23.c
+++ M23.c
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *fp1 = fopen("test.txt", "r");
+    FILE *fp1 = fopen("gr.txt", "r");
     FILE *fp2 = fopen("im.gv", "w");
 
-    printf("%s", "Hello GIT!");
+    char* a = (char*)malloc(1*sizeof(char));
+    int i = 0;
+    char c;
 
+    while ((c = fgetc(fp1))!=EOF) {
+        a[i++] = c;
+        a = (char*)realloc(a, (i+1)*sizeof(char));
+        if (a == NULL) {
+            exit -1;
+        }
+    }
+
+    for (int j = 0; j < i; j++) {
+        printf("%c",a[j]);
+    }
+    
+
+    free(a);
     fclose(fp1);
     fclose(fp2);
 
