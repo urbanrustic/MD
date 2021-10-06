0adf848af375eaecde465cb9a914f84e94012b56
urbanrustic <ankershtok77@gmail.com>
2021-10-06 18:34:04 +0300

    test

--- M23.c
+++ M23.c
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 
 int main() {
+    FILE *fp1 = fopen("test.txt", "r");
+    FILE *fp2 = fopen("im.gv", "w");
+
     printf("%s", "Hello GIT!");
+
+    fclose(fp1);
+    fclose(fp2);
+
 }
