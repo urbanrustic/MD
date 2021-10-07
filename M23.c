544159981aa853ec0c4776248a7c0c3f0775b203
urbanrustic <ankershtok77@gmail.com>
2021-10-07 23:45:27 +0300

    penultimate

--- M23.c
+++ M23.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+
+void DFC(int param, int length, int *arr, int *visited) {
+    if (visited[param]) {
+        return;
+    }
+    visited[param] = 1;
+    for (int i = 1; i < length; i++) {
+        if ((visited[i] == 0) && (arr[param*length + i] == 1)) {
+            DFC(i, length, arr, visited);
+        }
+        
+    }
+}
+
 //Матрица смежности 
-void matr(int apex, FILE *fp1) {
+int matr(int apex, FILE *fp1) {
     //Создаём массив нулей
-    int arr[apex+1][apex+1];
+    int ar[apex+1][apex+1];
     for (int c = 0; c < apex+1; c++) {
         for (int b = 0; b < apex+1; b++) {
-            arr[c][b] = 0;
+            ar[c][b] = 0;
         }   
     }
 
@@ -24,24 +38,53 @@ void matr(int apex, FILE *fp1) {
         ///////////////
         if (isdigit(c) && fl) {
             k = c-'0';
+            printf("%d ", k);
         }
         else if (isdigit(c)) {
             v = c-'0';
+            printf("%d ", v);
         }
         ///////////////
         if (v && k && fl) {
-            if (v < k) {
-                arr[v][k] = 1;
-            }
-            else {
-                arr[k][v] = 1;
-            }
-                fl = 0;
-                v = 0;
-                k = 0;
+            ar[v][k] = 1;
+            ar[k][v] = 1;
+            fl = 0;
+            v = 0;
+            k = 0;
         }
     }
-    
+    for (int i = 0; i < apex+1; i++) {
+        for (int j = 0; j < apex+1; j++) {
+            printf("%d", ar[i][j]);
+        }
+        printf("\n");
+    }
+
+    int *arr = &ar[0][0];
+    int visited[apex+1];
+    for (int i = 0; i < apex+1; i++) {
+        visited[i] = 0;
+    }
+    printf("\n");
+    DFC(apex, apex+1, arr, visited);
+
+    for (int i = 1; i < apex+1; i++) {
+        printf("%d",visited[i]);
+    }
+    printf("\n");
+
+    int flag = 0;
+    for (int i = 1; i < apex+1; i++) {
+        if (visited[i] == 1) {
+            flag++;
+        }
+    }
+    if (flag == apex) {
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 
@@ -84,12 +127,19 @@ int main() {
     }
     fprintf(fp2,"%s","\n}");
     
-    printf("Вершин %d Рёбер %d\n", apex, edges);
+    printf("%d %d\n", apex, edges);
     
 
     free(a);
 
-    matr(apex, fp1);
+    if ((apex-edges == 1) && matr(apex, fp1)) {
+        printf("\n%s\n", "Дерево");
+    }
+    else {
+        printf("\n%s\n","Не является деревом");
+    }
+    
+    
     
     fclose(fp1);
     fclose(fp2);
