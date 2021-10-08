0dd8949ec738657e5684d9e62b69ae97ea98c401
urbanrustic <ankershtok77@gmail.com>
2021-10-08 09:23:10 +0300

    done

--- M23.c
+++ M23.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
+//Функция для выявления связанных вершин
 void DFC(int param, int length, int *arr, int *visited) {
+
     if (visited[param]) {
         return;
     }
@@ -18,60 +19,60 @@ void DFC(int param, int length, int *arr, int *visited) {
 
 //Матрица смежности 
 int matr(int apex, FILE *fp1) {
-    //Создаём массив нулей
+
     int ar[apex+1][apex+1];
-    for (int c = 0; c < apex+1; c++) {
-        for (int b = 0; b < apex+1; b++) {
-            ar[c][b] = 0;
+    for (int i = 0; i < apex+1; i++) {
+        for (int j = 0; j < apex+1; j++) {
+            ar[i][j] = 0;
         }   
     }
 
-    int fl = 0;
+    int fl1 = 0, fl2 = 0;
     int v = 0, k = 0;
     char c;
+    //Возвращение к началу файла
     rewind(fp1);
 
     while ((c = fgetc(fp1))!=EOF) {
-        if (c == '-') {
-            fl = 1;
-        }
-        ///////////////
-        if (isdigit(c) && fl) {
-            k = c-'0';
-            printf("%d ", k);
-        }
-        else if (isdigit(c)) {
-            v = c-'0';
-            printf("%d ", v);
-        }
-        ///////////////
-        if (v && k && fl) {
+        if (v && k && fl1 && fl2) {
             ar[v][k] = 1;
             ar[k][v] = 1;
-            fl = 0;
+            fl1 = 0;
+            fl2 = 0;
             v = 0;
             k = 0;
         }
-    }
-    for (int i = 0; i < apex+1; i++) {
-        for (int j = 0; j < apex+1; j++) {
-            printf("%d", ar[i][j]);
+
+        if (c == '\n') {
+            fl2 = 1;
         }
-        printf("\n");
+        if (c == '-') {
+            fl1 = 1;
+        }
+        if (isdigit(c) && fl1 && !fl2) {
+            k = c-'0' + k*10;
+        }
+        else if (isdigit(c)) {
+            v = c-'0' + v*10;
+        }
     }
 
+    if (v && k && fl1) {
+            ar[v][k] = 1;
+            ar[k][v] = 1;
+            fl1 = 0;
+            v = 0;
+            k = 0;
+        }   
+
+    //Указатель на первый символ для матрицы смежности
     int *arr = &ar[0][0];
     int visited[apex+1];
     for (int i = 0; i < apex+1; i++) {
         visited[i] = 0;
     }
-    printf("\n");
-    DFC(apex, apex+1, arr, visited);
 
-    for (int i = 1; i < apex+1; i++) {
-        printf("%d",visited[i]);
-    }
-    printf("\n");
+    DFC(apex, apex+1, arr, visited);
 
     int flag = 0;
     for (int i = 1; i < apex+1; i++) {
@@ -79,67 +80,73 @@ int matr(int apex, FILE *fp1) {
             flag++;
         }
     }
+
     if (flag == apex) {
         return 1;
     }
-    else{
+    else {
         return 0;
     }
 }
-
 
 
 int main() {
     FILE *fp1 = fopen("gr.txt", "r");
     FILE *fp2 = fopen("im.gv", "w");
 
-    char* a = (char*)malloc(1*sizeof(char));
+    char* graf = (char*)malloc(1*sizeof(char));
     int i = 0, edges = 0, apex = 0;
     char c;
 
     //Считываем из файла txt
     while ((c = fgetc(fp1))!=EOF) {
-        a[i] = c;
-        //количество рёбер и максимальная вершина
-        if (a[i] == '-') {
+        graf[i] = c;
+        //количество рёбер
+        if (graf[i] == '-') {
             edges++;
         }
-        if (isdigit(c) && (apex < (c-'0'))) {
-            apex = c-'0';
-        }
-        a = (char*)realloc(a, (++i+1)*sizeof(char));
-        if (a == NULL) {
+        graf = (char*)realloc(graf, (++i+1)*sizeof(char));
+        if (graf == NULL) {
             exit -1;
         }
     }
-
+    //Максимальная вершина
+    int count = 0;
+    int j = 0;
+    while (graf[j]) {
+        while (graf[j] && graf[j]!='-' && graf[j]!='\n' && isdigit(graf[j])) {
+            count = graf[j]-'0' + count*10;
+            j++;
+        }
+        if (count > apex) {
+            apex = count;
+        }
+        count = 0;
+        j++;
+    }
+    
     //Заполнение файла gv
     fprintf(fp2, "%s", "graph {\n\t");
     for (int j = 0; j < i; j++) {
-        fputc(a[j], fp2);
-        if (a[j] == '\n') {
+        fputc(graf[j], fp2);
+        if (graf[j] == '\n') {
             fputc('\t', fp2);
         }
-        if (a[j] == '-') {
+        if (graf[j] == '-') {
             fputc('-', fp2);
         }
         
     }
     fprintf(fp2,"%s","\n}");
     
-    printf("%d %d\n", apex, edges);
-    
-
-    free(a);
+    free(graf);
 
     if ((apex-edges == 1) && matr(apex, fp1)) {
-        printf("\n%s\n", "Дерево");
+        printf("%s\n", "Дерево");
     }
     else {
-        printf("\n%s\n","Не является деревом");
+        printf("%s\n","Не является деревом");
     }
-    
-    
     
     fclose(fp1);
     fclose(fp2);
